608c20bb736109761b736315215910eed38a0a48
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2023-05-18 15:02:22 +0300

    no message

---
+++ burcOgren/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  burcOgren
+//
+//  Created by Furkan Erzurumlu on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
