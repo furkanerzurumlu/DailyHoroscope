7c169f47ecd18398b2c7e64f75df5abab7b1efa1
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2023-05-18 17:30:30 +0300

    no message

--- burcOgren/ViewController.swift
+++ burcOgren/ViewController.swift
@@ -6,18 +6,74 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var emailField: UITextField!
     
+    @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var contiueButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setField(field: emailField, placeholder: "Email Address")
+        setField(field: passwordField, placeholder: "Password")
+        contiueButton.setTitle("Başla", for: UIControl.State.normal)
+        setButton(button: contiueButton)
     }
-
-
+    
+    func setField(field:UITextField,placeholder:String){
+        field.layer.cornerRadius = 20
+        field.layer.borderWidth = 0.5
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = placeholder
+        
+    }
+    
+    func setButton(button:UIButton){
+        button.layer.cornerRadius = 20
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        emailField.becomeFirstResponder()
+    }
+    
+    @IBAction func nextPageAction(_ sender: Any) {
+        print("Action")
+        guard let email = emailField.text, !email.isEmpty,
+              let password = passwordField.text, !password.isEmpty else{
+            print("Missing field data")
+            return
+        }
+        
+        //Get auth instance
+        //attempt sign in
+        //if failure, present alert to create account
+        //if user continues, create account
+        
+        //check sign in on app launch
+        //allow user to sign out with button
+        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password,completion: { [weak self] result,error in
+            guard let strongSelf = self else {
+                return
+            }
+            guard error == nil else {
+                // show account creation
+                strongSelf.showCreateAccont()
+                return
+            }
+            print("You have signed in")
+        })
+        
+    }
+    func showCreateAccont(){
+        
+    }
 }
 
