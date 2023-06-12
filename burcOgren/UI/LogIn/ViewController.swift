41d7e246b0ec6e41ef90585e60822d6a60f344e4
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2023-06-12 12:39:47 +0300

    Set loginVC

--- burcOgren/UI/LogIn/ViewController.swift
+++ burcOgren/UI/LogIn/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
         contiueButton.setTitle("Continue", for: UIControl.State.normal)
         setButton(button: contiueButton)
         
+        self.passwordField.setupLeftImage(imageName: "lock")
+        self.emailField.setupLeftImage(imageName: "message")
 //        if FirebaseAuth.Auth.auth().currentUser != nil {
 //
 //        }
@@ -114,3 +116,30 @@ class ViewController: UIViewController {
     }
 }
 
+extension UITextField {
+
+    //MARK:- Set Image on the right of text fields
+
+  func setupRightImage(imageName:String){
+    let imageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 20, height: 20))
+    imageView.image = UIImage(named: imageName)
+    let imageContainerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 55, height: 40))
+    imageContainerView.addSubview(imageView)
+    rightView = imageContainerView
+    rightViewMode = .always
+    self.tintColor = .lightGray
+}
+
+ //MARK:- Set Image on left of text fields
+
+    func setupLeftImage(imageName:String){
+       let imageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 20, height: 20))
+       imageView.image = UIImage(named: imageName)
+       let imageContainerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 40))
+       imageContainerView.addSubview(imageView)
+       leftView = imageContainerView
+       leftViewMode = .always
+       self.tintColor = .lightGray
+     }
+
+  }
